2a13c5c5c246ad374dc568ac41cd11bfd5702ce9
Patrick Diehl <diehl@ins.uni-bonn.de>
2016-04-27 14:48:38 +0200

    Example for documentation with pdoc

--- dic_reader_2D.py
+++ dic_reader_2D.py
@@ -10,6 +10,9 @@ import csv
 import numpy as np
 
 class dic_reader_2D():
+  """
+  A class for reading an providing files from 2 dimensional dic
+  """
  
   def __init__(self,path):
     self.data = []
@@ -17,22 +20,21 @@ class dic_reader_2D():
     self.sortdata()
   
   def read(self,path):
+      """
+      Read the values provided by the dic and sotres it to the data array
+      
+      `path`  Path and appended file name for the csv file to proced
+      """
       with open(path,'rb') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
 	next(csvreader,None)
 	for row in csvreader:
 	   self.data.append(np.array(map(float, row)))
 	    
-  def sortX(self,item):
-      return item[0]
-      
-  def sortY(self,item):
-      return item[1]
-  
-  def sortZ(self,item):
-      return item[2]
-      
   def sortdata(self):
+    """
+    Sorts the data from the csv file with respect to the first and second pixel
+    """
     self.data.sort(key = lambda x: (x[13], x[14]))
     print len(self.data)
     for i in range(0,len(self.data)):
