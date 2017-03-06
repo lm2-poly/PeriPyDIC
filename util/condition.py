fa220704096fb694f246ab3d7cdf4c955766ed22
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-03-06 11:22:55 -0500

    Add missing file

---
+++ util/condition.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import csv
+import numpy as np
+
+class Condition():
+
+    def __init__(self, data):
+        self.type = data["Type"]
+        self.value = data["Value"]
+        self.id = self.readCondition(data["File"])
+
+    def readCondition(self, inFile):
+        if not os.path.exists(inFile):
+            print "Error: Could not find " + inFile
+            sys.exit(1)
+
+	with open(inFile, 'r') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=' ')
+		# Skip the first line, because is the header
+		next(spamreader)
+		length = len(list(spamreader))
+		id = np.empty(length)
+		csvfile.seek(0)
+		next(spamreader)
+		i = 0
+		for row in spamreader:
+			id[i] = int(row[0])
+			i += 1
+        return id
