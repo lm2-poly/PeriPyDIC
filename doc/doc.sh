3dffc69ed4117cc084436023423b16f3eb913ce3
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-03-13 14:40:22 -0400

    Add missing files

---
+++ doc/doc.sh
@@ -0,0 +1,3 @@
+sed ' 1 s/.*/&{#mainpage}/' ../README.md > README.md
+
+doxygen configuration.doxy
--- doc/html/search/pages_0.js
+++ doc/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['the_20problem',['The problem',['../index.html',1,'']]]
+  ['readme',['README',['../md_README.html',1,'']]]
 ];
