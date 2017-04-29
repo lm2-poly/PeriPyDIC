e3a3ac83a48688cfe7cc6469761a61ad5540190e
Ilyass Tabiai <joydisee@gmail.com>
2017-04-29 15:53:36 -0400

    Testing delete moved file method

--- materials/__init__.py
+++
@@ -1,8 +0,0 @@
-##@package materials
-# Provides the different materials
-#@author: ilyass.tabiai@polymtl.ca
-#@author: rolland.delorme@polymtl.ca
-#@author: patrick.diehl@polymtl.ca
-
-from .elastic import *
-from .viscoelastic import *
