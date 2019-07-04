31dffe6a7e3c400f1c598a877341f97aa3bdb88b
Patrick Diehl <me@diehlpk.de>
2019-07-03 19:31:57 -0500

    remove warnings form the install script

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='PeriPyDIC',
       url='https://github.com/lm2-poly/PeriPyDIC',
       keywords='material science, peridynamics, digital image correlation',
       license='GPL-3.0',
-      packages=find_namespace_packages(),
+      packages=find_namespace_packages(where="./"),
       zip_safe=False)
