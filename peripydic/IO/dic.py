fd0e145ce0be4227b4cc7e8557133ebf3847cc80
Patrick <patrickdiehl@lsu.edu>
2020-07-14 20:55:35 -0500

    Fix format

--- peripydic/IO/dic.py
+++ peripydic/IO/dic.py
@@ -114,16 +114,16 @@ class DICreader2D():
                 self.strain[i][2] = self.data[i][10]
 
 
-          del self.data
-          ## Nodes initial positions
-          self.nodes = np.empty((self.length, self.dim))
-          self.nodes[:,0] = self.x -dx
-          if self.dim == 2:
-            self.nodes[:,1] = self.y -dy
-          ## Nodes actual positions
-          self.act = np.empty((self.length, self.dim))
-          self.act[:,0] = self.x  #+ dx
-          if self.dim == 2:
-            self.act[:,1] = self.y #+ dy
+            del self.data
+            ## Nodes initial positions
+            self.nodes = np.empty((self.length, self.dim))
+            self.nodes[:,0] = self.x -dx
+            if self.dim == 2:
+                self.nodes[:,1] = self.y -dy
+            ## Nodes actual positions
+            self.act = np.empty((self.length, self.dim))
+            self.act[:,0] = self.x  #+ dx
+            if self.dim == 2:
+                self.act[:,1] = self.y #+ dy
 
 
